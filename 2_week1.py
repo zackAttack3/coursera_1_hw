66d6cc597da0362934fc2d488acd5e2aa05a05c7
zack1 <sw@MacBook-Pro-2.local>
2019-07-22 23:45:56 -0700

    2_week1 recursive dfs works (crashes for large files) - iterative dfs needs to be fixed

--- 2_week1.py
+++ 2_week1.py
@@ -23,51 +23,85 @@ class Node:
         for n_i in g.values():
             #print(n_i.vertex,n_i.visited,[i.vertex for i in n_i.heads])
             print(n_i,n_i.heads)
-        pass
+        return None
+    
+    @staticmethod
+    def sscs(g):
+        ssc={}
+        for n_i in g.values():
+            l_i=n_i.leader
+            ssc_n=ssc.get(l_i,0)
+            ssc[l_i]=ssc_n+1     
+        return ssc
     
 
 
 def dfs(graph,node,leader,t,f):
     nodeStack=[]
     nodeStack.append(node)
-    Node.printStatus(graph)
-    node.leader = leader[0]
+    #Node.printStatus(graph)
+    
 
     while nodeStack:
-        print([i.vertex for i in nodeStack])
+        #print([i.vertex for i in nodeStack])
         node=nodeStack.pop(-1)
         node.visited=True
         t[0]+=1
-        f[t[0]]=node.vertex
+        node.leader = leader[0]
+        
         for head_i in node.heads:
             if not head_i.visited:
                 nodeStack.append(head_i)
+        if nodeStack:
+            f.append(nodeStack[-1].vertex)
     
     return None
 
+def dfs2(graph,node,leader,t,f):
+    if node.visited:
+        return None
+    node.visited=True
+    node.leader=leader[0]
+    for n_i in node.heads:
+        if not n_i.visited:
+            dfs2(graph,n_i,leader,t,f)
+    t[0]+=1
+    f.append(node.vertex)
+    #print(t,node.vertex)
 
 graph={}
 graph_r={}
-with open('SCC_2.txt', 'r') as f:
+with open('SCC.txt', 'r') as f:
     for row in f:
+        
         edge=[int(i) for i in row.split()]
-        Node.addNode(graph,edge)
-        edge.reverse()
-        Node.addNode(graph_r,edge)
-
+        if len(edge)>0:
+            Node.addNode(graph,edge)
+            edge.reverse()
+            Node.addNode(graph_r,edge)
+import sys
+sys.setrecursionlimit(1000000)
 t=[0]
-f={}
+f=[]
 leader=[None]
 for v_i in graph_r.values():
     if not v_i.visited:
         
         leader[0]=v_i
-        Node.printStatus(graph)
-        print(v_i.vertex,'----------')
-        dfs(graph_r,v_i,leader,t,f)
+        #Node.printStatus(graph)
+        #print(v_i.vertex,'----------')
+        dfs2(graph_r,v_i,leader,t,f)
+print(f[:5])
+f.reverse()
+t=[0]
+for f_i in f:
+    v_i=graph.get(f_i,None)
+    if not v_i.visited:
+        leader[0]=v_i
+        #Node.printStatus(graph)
+        #print(v_i.vertex,'----------')
+        dfs2(graph,v_i,leader,t,f)
 
-for v_i in graph_r.values():
-    #print(v_i.vertex,[i.vertex for i in v_i.heads])
-    pass
 
-print(f)
+
+print(sorted(Node.sscs(graph).values(),reverse=True)[:5])
