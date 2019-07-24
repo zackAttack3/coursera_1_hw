e834779521142dc9c29f671c5f5119e7513eafea
zack1 <sw@MacBook-Pro-2.local>
2019-07-23 22:02:06 -0700

    solved 2_week1.py

--- 2_week1.py
+++ 2_week1.py
@@ -1,3 +1,15 @@
+'''
+1
+
+The file contains the edges of a directed graph. Vertices are labeled as positive integers from 1 to n (n ≤ 875714). Every row indicates an edge, the vertex label in first column is the tail and the vertex label in second column is the head (recall the graph is directed, and the edges are directed from the first column vertex to the second column vertex). So for example, the 11th row looks like : "2 47646". This just means that the vertex with label 2 has an outgoing edge to the vertex with label 47646
+
+Your task is to code up the algorithm from the video lectures for computing strongly connected components (SCCs), and to run this algorithm on the given graph.
+
+Output Format: You should output the sizes of the 5 largest SCCs in the given graph, in decreasing order of sizes, separated by commas (avoid any spaces). So if your algorithm computes the sizes of the five largest SCCs to be 500, 400, 300, 200 and 100, then your answer should be "500,400,300,200,100" (without the quotes). If your algorithm finds less than 5 SCCs, then write 0 for the remaining terms. Thus, if your algorithm computes only 3 SCCs whose sizes are 400, 300, and 100, then your answer should be "400,300,100,0,0" (without the quotes). (Note also that your answer should not have any spaces in it.)
+
+WARNING: This is the most challenging programming assignment of the course. Because of the size of the graph you may have to manage memory carefully. The best way to do this depends on your programming language and environment, and we strongly suggest that you exchange tips for doing this on the discussion forums.
+'''
+
 class Node:
     def __init__(self, value):
         self.vertex = value
@@ -46,14 +58,14 @@ def dfs(graph,node,leader,t,f):
         #print([i.vertex for i in nodeStack])
         node=nodeStack.pop(-1)
         node.visited=True
+        f[-1].append(node.vertex)
         t[0]+=1
         node.leader = leader[0]
         
         for head_i in node.heads:
             if not head_i.visited:
                 nodeStack.append(head_i)
-        if nodeStack:
-            f.append(nodeStack[-1].vertex)
+                
     
     return None
 
@@ -90,17 +102,21 @@ for v_i in graph_r.values():
         leader[0]=v_i
         #Node.printStatus(graph)
         #print(v_i.vertex,'----------')
-        dfs2(graph_r,v_i,leader,t,f)
-print(f[:5])
-f.reverse()
+        f.append([])
+        dfs(graph_r,v_i,leader,t,f)
+[fi.reverse() for fi in f]
+f2=[]
+[f2.extend(fi) for fi in f] 
+
+f2.reverse()
 t=[0]
-for f_i in f:
+for f_i in f2:
     v_i=graph.get(f_i,None)
     if not v_i.visited:
         leader[0]=v_i
         #Node.printStatus(graph)
         #print(v_i.vertex,'----------')
-        dfs2(graph,v_i,leader,t,f)
+        dfs(graph,v_i,leader,t,f)
 
 
 
